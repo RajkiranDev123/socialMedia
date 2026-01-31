be83afde6de331ceca18333a5f78a7018fe83cc7
rajkiran <rajkiran@tekonika.co>
2026-01-31 16:17:25 +0530

    login api

--- server/index.js
+++ server/index.js
@@ -1,14 +1,34 @@
-import dotenv from "dotenv";
 import express from "express";
-import {databaseConnection} from "./config/database.js"
+import dotenv from "dotenv";
+import {databaseConnection} from "./config/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
+// import tweetRoute from "./routes/tweetRoute.js";
+import cors from "cors";
+
 dotenv.config({
-  path: ".env",
-});
-const app = express();
-const PORT = process.env.PORT;
+    path:".env"
+})
+databaseConnection();
+const app = express(); 
 
-databaseConnection()
+// middlewares
+app.use(express.urlencoded({
+    extended:true
+}));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+    origin:"http://localhost:3000",
+    credentials:true
+}
+app.use(cors(corsOptions));
 
-app.listen(PORT, () => {
-  console.log(`Server is listening at ${PORT}`);
-});
+// api
+app.use("/api/v1/user",userRoute);
+// app.use("/api/v1/tweet", tweetRoute);
+ 
+
+app.listen(process.env.PORT,() => {
+    console.log(`Server listen at port ${process.env.PORT}`);
+})
